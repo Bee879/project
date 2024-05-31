a63f7f87b54a6f2350b44955894f2cc1f7ee1d2a
Bee879 <129519179+Bee879@users.noreply.github.com>
2024-05-31 21:03:02 +0200

    Delete project directory

--- project/processing.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Simulate processing delay
-sleep(3); // Delay for 3 seconds
-
-// Redirect to fake processing page
-header("Location: processing_fake.php");
-exit; // Ensure no further code is executed after redirection
-?>
